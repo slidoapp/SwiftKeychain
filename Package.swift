b3b9e835a072164b58a6588cfa683acaf5873370
Jozef Izso <jozef.izso@gmail.com>
2023-05-15 22:08:54 +0200

    Create package for SwiftKeychain library
    Targets Swift 3.0 and Xcode 8.2

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:3.0
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftKeychain"
+    // platforms: [
+    //     .macOS(.v10_10),
+    //     .iOS(.v8),
+    //     .watchOS(.v2),
+    //     .tvOS(.v9)
+    // ]
+)
