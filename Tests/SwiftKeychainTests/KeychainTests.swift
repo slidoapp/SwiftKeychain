5da72d4a70346ec78df60d63933a6fb1c1215b32
Jozef Izso <izso@cisco.com>
2024-11-03 15:30:51 +0100

    Make items with unique names to enable parallel testing

--- Tests/SwiftKeychainTests/KeychainTests.swift
+++ Tests/SwiftKeychainTests/KeychainTests.swift
@@ -25,7 +25,7 @@ class KeychainTests: XCTestCase {
     
     func testInsertItemWithAttributes() {
         
-        let item = MockGenericPasswordItem(accountName: "John")
+        let item = MockGenericPasswordItem(accountName: "John_testInsertItemWithAttributes")
         let keychain = Keychain()
         var hasError = false
         
@@ -61,7 +61,7 @@ class KeychainTests: XCTestCase {
     
     func testRemoveItemWithAttributes() {
         
-        let item = MockGenericPasswordItem(accountName: "John")
+        let item = MockGenericPasswordItem(accountName: "John_testRemoveItemWithAttributes")
         let keychain = Keychain()
         var hasError = false
         
@@ -99,7 +99,7 @@ class KeychainTests: XCTestCase {
     
     func testFetchItemWithAttributes() {
         
-        let item = MockGenericPasswordItem(accountName: "John")
+        let item = MockGenericPasswordItem(accountName: "John_testFetchItemWithAttributes")
         let keychain = Keychain()
         var hasError = false
         var fetchedToken = ""
@@ -145,7 +145,7 @@ class KeychainTests: XCTestCase {
     
     func testFetchItemWithAttributesReturnsNilIfResultIsNotADictionary() {
         
-        let item = MockGenericPasswordItem(accountName: "John")
+        let item = MockGenericPasswordItem(accountName: "John_testFetchItemWithAttributesReturnsNilIfResultIsNotADictionary")
         let keychain = Keychain()
         
         try! keychain.insertItemWithAttributes(item.attributes)
